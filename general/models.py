1f6ca03f1d198a7c2bf39cba22ba8314bea3af46
johnathanrs <johnathans23@yahoo.com>
2018-08-25 04:02:50 +0530

    revise roto scraper, add player model

--- general/models.py
+++ general/models.py
@@ -3,4 +3,46 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+DATA_SOURCE = (
+    ('FanDuel', 'FanDuel'),
+    ('DraftKing', 'DraftKing'),
+    ('Yahoo', 'Yahoo'),
+    ('Head2Head', 'Head2Head'),
+    ('Fanball', 'Fanball'),
+    ('FantasyDraft', 'FantasyDraft')
+)
+
+class Player(models.Model):
+    uid = models.IntegerField()
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    injury = models.TextField(blank=True, null=True)
+    minutes = models.FloatField()
+    money_line = models.IntegerField()
+    opponent = models.CharField(max_length=50)
+    over_under = models.FloatField()
+    point_spread = models.FloatField()
+    position = models.CharField(max_length=50)
+    proj_ceiling = models.FloatField()
+    proj_custom = models.FloatField()
+    proj_floor = models.FloatField()
+    proj_original = models.FloatField()
+    proj_points = models.FloatField()
+    proj_rotowire = models.FloatField()
+    proj_site = models.FloatField()
+    proj_third_party_one = models.FloatField()
+    proj_third_party_two = models.FloatField()
+    real_position = models.CharField(max_length=50, blank=True, null=True)
+    salary = models.FloatField()
+    salary_custom = models.FloatField()
+    salary_original = models.FloatField()
+    team = models.CharField(max_length=50)
+    team_points = models.FloatField()
+    value = models.FloatField()
+    data_source = models.CharField(max_length=30, choices=DATA_SOURCE, default='FanDuel')
+
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return '{} {}'.format(self.first_name, self.last_name)
--- scripts/roto.py
+++ scripts/roto.py
@@ -1,54 +1,32 @@
-import csv
 import requests
-
-from bs4 import BeautifulSoup
-from operator import itemgetter
+import csv
 
 import os
 from os import sys, path
 import django
 
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fantasy_soccer.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fantasy_nba.settings")
 django.setup()
 
 from general.models import *
 
 def main():
-    dp = "https://www.rotowire.com/daily/soccer/optimizer.php"
+    try:
+        url = 'https://www.rotowire.com/daily/tables/optimizer-nba.php?sport=NBA&site=FanDuel&projections=&type=main&slate=all'
+        url = 'https://www.rotowire.com/daily/tables/optimizer-soc.php?league=EPL&site=FanDuel&projections=&type=main&slate=all'
+        players = requests.get(url).json()
 
-    session = requests.Session()
-    session.headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.2228.0 Safari/537.36'})
+        with open('soc-players.csv', 'wb') as f:
+            fields = players[0].keys()
+            print fields
+            w = csv.DictWriter(f, fields)
+            w.writeheader()
 
-    r = session.get(dp)
-
-    if r.status_code == 200:
-        soup = BeautifulSoup(r.text, "html.parser")
-
-        try:
-            table = soup.find("div", {"id":"rwo-poolbox"})
-            player_rows = table.find("tbody", {"id":"players"})
-            players = player_rows.find_all("tr")
-        except Exception as e:
-            return  # no players
-
-        for player in players:
-            try:
-                defaults = {
-                    'salary': int(player.find("td", {"class":"rwo-salary"}).get('data-salary').replace(",","")),
-                    'position': player.find("td", {"class":"rwo-pos"}).get('data-li'),
-                    'points': float(player.find("td", {"class":"rwo-points"}).get('data-points')),
-                    'value': float(player.find("td", {"class":"rwo-value"}).text)
-                }
-
-                obj = Player.objects.update_or_create(
-                    name = player.find("td", {"class":"rwo-name"}).text.strip().replace('\n',' '),
-                    team=player.find("td", {"class":"rwo-team"}).text,
-                    defaults=defaults
-                )
-            except (Exception) as e:
-                print (e)
-        
+            for ii in players:
+                w.writerow(ii)
+    except:
+        pass
 
 if __name__ == "__main__":
     main()
