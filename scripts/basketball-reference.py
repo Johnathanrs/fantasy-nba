241eb2196a3c625b22d1a567a5f4b46da98c9fa2
johnathanrs <johnathans23@yahoo.com>
2018-09-08 01:47:32 +0530

    finish scraper

--- scripts/basketball-reference.py
+++ scripts/basketball-reference.py
@@ -8,6 +8,8 @@ from os import sys, path
 import django
 import pdb
 
+from datetime import datetime
+
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fantasy_nba.settings")
 django.setup()
@@ -23,6 +25,12 @@ def main():
     # pdb.set_trace()
 
     try:
+        date = soup.find("span", {"class": "button2 current"}).text
+        date = datetime.strptime(date, '%b %d, %Y')
+        last_game = PlayerGame.objects.all().order_by('-date').first()
+        if last_game and last_game.date == date.date():
+            return
+
         table = soup.find("table", {"id":"stats"})
         player_rows = table.find("tbody")
         players = player_rows.find_all("tr")
@@ -32,38 +40,33 @@ def main():
     for player in players:
         try:
             if not player.get('class'): # ignore header
-                name = player.find("td", {"data-stat":"player"}).text
-                team = player.find("td", {"data-stat":"team_id"}).text
-                location = player.find("td", {"data-stat":"game_location"}).text
-                opp = player.find("td", {"data-stat":"opp_id"}).text
-                game_result = player.find("td", {"data-stat":"game_result"}).text
-                
-                mp = player.find("td", {"data-stat":"mp"}).text
-                fg = player.find("td", {"data-stat":"fg"}).text
-                fga = player.find("td", {"data-stat":"fga"}).text
-                fg_pct = player.find("td", {"data-stat":"fg_pct"}).text
-                fg3 = player.find("td", {"data-stat":"fg3"}).text
-                fg3a = player.find("td", {"data-stat":"fg3a"}).text
-                fg3_pct = player.find("td", {"data-stat":"fg3_pct"}).text
-                ft = player.find("td", {"data-stat":"ft"}).text
-                fta = player.find("td", {"data-stat":"fta"}).text
-                ft_pct = player.find("td", {"data-stat":"ft_pct"}).text
-                orb = player.find("td", {"data-stat":"orb"}).text
-                drb = player.find("td", {"data-stat":"drb"}).text
-                trb = player.find("td", {"data-stat":"trb"}).text
-                ast = player.find("td", {"data-stat":"ast"}).text
-                stl = player.find("td", {"data-stat":"stl"}).text
-                blk = player.find("td", {"data-stat":"blk"}).text
-                tov = player.find("td", {"data-stat":"tov"}).text
-                pf = player.find("td", {"data-stat":"pf"}).text
-                pts = player.find("td", {"data-stat":"pts"}).text
-                print name, team_id, game_location, opp_id, game_result, mp, fg, fga, fg_pct, fg3, fg3a, fg3_pct, ft, fta, ft_pct, orb, drb, trb, ast, stl, blk, tov, pf, pts
+                mp = player.find("td", {"data-stat":"mp"}).text.split(':')
 
-            # obj = Player.objects.update_or_create(
-            #     name = player.find("td", {"class":"rwo-name"}).text.strip().replace('\n',' '),
-            #     team=player.find("td", {"class":"rwo-team"}).text,
-            #     defaults=defaults
-            # )
+                obj = PlayerGame.objects.create(
+                    name = player.find("td", {"data-stat":"player"}).text,
+                    team = player.find("td", {"data-stat":"team_id"}).text,
+                    location = player.find("td", {"data-stat":"game_location"}).text,
+                    opp = player.find("td", {"data-stat":"opp_id"}).text,
+                    game_result = player.find("td", {"data-stat":"game_result"}).text,
+                    mp = float(mp[0])+float(mp[1])/60,
+                    fg = player.find("td", {"data-stat":"fg"}).text,
+                    fga = player.find("td", {"data-stat":"fga"}).text,
+                    fg_pct = player.find("td", {"data-stat":"fg_pct"}).text or None,
+                    fg3 = player.find("td", {"data-stat":"fg3"}).text,
+                    fg3a = player.find("td", {"data-stat":"fg3a"}).text,
+                    fg3_pct = player.find("td", {"data-stat":"fg3_pct"}).text or None,
+                    ft = player.find("td", {"data-stat":"ft"}).text,
+                    fta = player.find("td", {"data-stat":"fta"}).text,
+                    ft_pct = player.find("td", {"data-stat":"ft_pct"}).text or None,
+                    trb = player.find("td", {"data-stat":"trb"}).text,
+                    ast = player.find("td", {"data-stat":"ast"}).text,
+                    stl = player.find("td", {"data-stat":"stl"}).text,
+                    blk = player.find("td", {"data-stat":"blk"}).text,
+                    tov = player.find("td", {"data-stat":"tov"}).text,
+                    pf = player.find("td", {"data-stat":"pf"}).text,
+                    pts = player.find("td", {"data-stat":"pts"}).text,
+                    date = date
+                )
         except (Exception) as e:
             print (e)
     
