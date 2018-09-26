26cecb5244553d0ffb4b9d7a04279f3e9ca9c5ce
johnathanrs <johnathans23@yahoo.com>
2018-09-27 01:42:12 +0530

    complete game slate on team match up board

--- static/js/team_matchup.js
+++ static/js/team_matchup.js
@@ -1,23 +1,21 @@
 $(document).ready(function () {
-  $('.tab-pane.slate input').on('change', function() {
-    loadBoard();
+  $('.game-item').on('click', function() {
+    $('.game-item').removeClass('active');
+    $(this).addClass('active');
+    loadBoard($(this).data('game'));
   });
 
+  $('.nav-tabs.slate a').on('shown.bs.tab', function(event) {
+    var slate = $(event.target).text();
+    $('#tab-'+slate+' .game-item:first').click();
+  });
+  
   $('.nav-tabs.slate .nav-link:first').click();
 })
 
-function loadBoard() {
-  games = '';
-  $('#tab-'+slate).find('input:checked').each(function() {
-    games += $(this).val()+';';
-  })
-
-  var data = { 
-        games: games
-      };
-
+function loadBoard(game) {
   $('.team-board').html('<div class="board-loading ml-1 mt-5">Loading ...</div>');
-  $.post( "/team-match-up", data, function( data ) {
+  $.post( "/team-match-up", { game: game }, function( data ) {
     $('.team-board').html(data);
   });
 }
