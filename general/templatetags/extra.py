7ff3257a79fe71714402f7e3ba1ffdcb217dd273
johnathanrs <johnathans23@yahoo.com>
2018-11-10 23:57:57 +0530

    revise style

--- general/templatetags/extra.py
+++ general/templatetags/extra.py
@@ -14,7 +14,12 @@ def liked(uid):
 
 @register.filter 
 def hot_sfp(player):
-    return 'text-danger' if player['sfp'] >= player['afp'] + 5 else 'text-primary' 
+	if player['sfp'] >= player['afp'] + 5:
+    	return 'text-danger'  
+    elif player['sfp'] <= player['afp'] - 5:
+    	return 'text-primary'
+	else:
+		return '' 
 
 @register.filter
 def ou_ml(game, team):
