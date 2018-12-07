de442bd036f52e6ce1d4e8fea41f0750235e34af
johnathanrs <johnathans23@yahoo.com>
2018-12-08 03:14:40 +0530

    revise fav dialog

--- static/js/fav_dlg.js
+++ static/js/fav_dlg.js
@@ -3,11 +3,20 @@ $('.fav-title').on('click', function() {
 });
 
 $('body').on('click','.fav',function() {
-  var self = this;
-  $.post( "/fav-player", { uid: $(this).data('uid') }, function( data ) {
-    $(self).toggleClass('done');
+  var uid = $(this).data('uid');
+  $.post( "/fav-player", { uid: uid }, function( data ) {
+    $('*[data-uid="'+uid+'"]').toggleClass('done');
     $('.fav-body').html(data);
   });
+});
+
+$('body').on('click','.fav-remove',function() {
+  var r = confirm("Are you sure to remove all favorites?");
+  if (r == true) {
+    $.post( "/fav-player", { uid: -1 }, function( data ) {
+      location.reload();
+    });
+  }
 });
 
 $(document).ready(function () {
