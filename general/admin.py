fa4d53b5fba9bc49594a386ba4e8bf1a2460eccb
johnathanrs <johnathans23@yahoo.com>
2019-01-15 04:17:53 +0530

    add export csv to player

--- general/admin.py
+++ general/admin.py
@@ -17,11 +17,46 @@ from rangefilter.filter import DateRangeFilter
 
 from general.models import *
 
+
+def write_report(queryset, path, result_csv_fields):
+    result = open(path, 'w')
+    result_csv = csv.DictWriter(result, fieldnames=result_csv_fields)
+    result_csv.writeheader()
+
+    for game in queryset:
+        game_ = model_to_dict(game, fields=result_csv_fields)
+
+        try:
+            result_csv.writerow(game_)
+        except Exception, e:
+            print game_
+
+    result.close()
+
+
 class PlayerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'position', 'team', 'opponent', 'salary', 'play_today', 
                     'proj_points', 'data_source', 'created_at', 'updated_at', 'avatar']
     search_fields = ['first_name', 'last_name', 'team']
     list_filter = ['team', 'data_source', 'position', 'play_today']
+    actions = ['export_players']
+
+    def export_players(self, request, queryset):
+        selected = request.POST.getlist(admin.ACTION_CHECKBOX_NAME)
+        ids = ','.join([str(item.id) for item in queryset])
+        fields = ['first_name', 'last_name', 'position', 'team', 'salary', 'proj_points', 'data_source', 'avatar']
+
+        path = "/tmp/nba_players.csv"
+        write_report(queryset, path, fields)
+        wrapper = FileWrapper( open( path, "r" ) )
+        content_type = mimetypes.guess_type( path )[0]
+
+        response = HttpResponse(wrapper, content_type = content_type)
+        response['Content-Length'] = os.path.getsize( path ) 
+        response['Content-Disposition'] = 'attachment; filename=%s' % smart_str( os.path.basename( path ) ) 
+        return response
+
+    export_players.short_description = "Export CSV" 
 
 
 class PlayerGameAdmin(admin.ModelAdmin):
@@ -39,7 +74,7 @@ class PlayerGameAdmin(admin.ModelAdmin):
                   if f.name not in ['id', 'is_new']]
 
         path = "/tmp/nba_games.csv"
-        self.write_report(queryset, path, fields)
+        write_report(queryset, path, fields)
         wrapper = FileWrapper( open( path, "r" ) )
         content_type = mimetypes.guess_type( path )[0]
 
@@ -50,24 +85,6 @@ class PlayerGameAdmin(admin.ModelAdmin):
 
     export_games.short_description = "Export CSV" 
 
-
-    def write_report(self, queryset, path, result_csv_fields):
-        result = open(path, 'w')
-        result_csv = csv.DictWriter(result, fieldnames=result_csv_fields)
-        result_csv.writeheader()
-
-        for game in queryset:
-            game_ = model_to_dict(game, fields=result_csv_fields)
-            # for key, val in game_.items():
-            #     if type(val) not in (float, int, long, bool, datetime.date) and val:
-            #         game_[key] = val.encode('utf-8')
-
-            try:
-                result_csv.writerow(game_)
-            except Exception, e:
-                print game_
-
-        result.close()
 
 class GameAdmin(admin.ModelAdmin):
     list_display = ['home_team', 'visit_team', 'ou', 'ml', 'game_status', 'date']
